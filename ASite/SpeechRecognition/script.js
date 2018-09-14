21f05276c3b9c1bd61bbde90ec728765f921a00c
ShadowForests <38903248+ShadowForests@users.noreply.github.com>
2018-09-14 16:34:44 -0700

    Update script.js

--- ASite/SpeechRecognition/script.js
+++ ASite/SpeechRecognition/script.js
@@ -6,15 +6,23 @@ var socket = io.connect('http://localhost:3000');
 var diagnosticPara = document.querySelector('.output');
 
 var testBtn = document.querySelector('button');
+var buttonState = 0;
 
-function randomPhrase() {
-  var number = Math.floor(Math.random() * phrases.length);
-  return number;
+function speechButton() {
+  if (buttonState < 1) {
+    buttonState = 1;
+    testBtn.textContent = 'Stop';
+    testSpeech();
+  } else {
+    buttonState = -1;
+    testBtn.disabled = true;
+    testBtn.textContent = 'Stopping...';
+  }
 }
 
 function testSpeech() {
-  testBtn.disabled = true;
-  testBtn.textContent = 'In progress';
+  //testBtn.disabled = true;
+  //testBtn.textContent = 'In progress';
 
   // To ensure case consistency while checking with the returned output text
   // diagnosticPara.textContent = '...diagnostic messages';
@@ -35,27 +43,32 @@ function testSpeech() {
     // These also have getters so they can be accessed like arrays.
     // The second [0] returns the SpeechRecognitionAlternative at position 0.
     // We then return the transcript property of the SpeechRecognitionAlternative object 
-    var speechResult = event.results[0][0].transcript;
-    diagnosticPara.textContent = 'Speech received: ' + speechResult + '. | Confidence: ' + event.results[0][0].confidence;
+    if (buttonState == 1) {
+      var speechResult = event.results[0][0].transcript;
+      diagnosticPara.textContent = 'Speech received: ' + speechResult + '. | Confidence: ' + event.results[0][0].confidence;
 
-    socket.emit('speech', speechResult);
+      socket.emit('speech', speechResult);
 
-    console.log('Confidence: ' + event.results[0][0].confidence);
+      console.log('Confidence: ' + event.results[0][0].confidence);
+    }
   }
 
   recognition.onspeechend = function() {
     recognition.stop();
-    testBtn.disabled = false;
-    running = false;
-    testBtn.textContent = 'Start new test';
-    testSpeech();
+    //testBtn.disabled = false;
+    //testBtn.textContent = 'Start';
+    if (buttonState == 1) {
+      testSpeech();
+    }
   }
 
   recognition.onerror = function(event) {
-    testBtn.disabled = false;
-    testBtn.textContent = 'Start new test';
-    diagnosticPara.textContent = 'Error occurred in recognition: ' + event.error;
-    testSpeech();
+    //testBtn.disabled = false;
+    //testBtn.textContent = 'Start';
+    if (buttonState == 1) {
+      diagnosticPara.textContent = 'Error occurred in recognition: ' + event.error;
+      testSpeech();
+    }
   }
   
   recognition.onaudiostart = function(event) {
@@ -66,12 +79,22 @@ function testSpeech() {
   recognition.onaudioend = function(event) {
       //Fired when the user agent has finished capturing audio.
       console.log('SpeechRecognition.onaudioend');
-      testSpeech();
+      if (buttonState == 1) {
+        testSpeech();
+      }
   }
   
   recognition.onend = function(event) {
       //Fired when the speech recognition service has disconnected.
       console.log('SpeechRecognition.onend');
+
+      if (buttonState == -1) {
+        console.log('SpeechRecognition.stopped');
+        buttonState = 0;
+        testBtn.textContent = 'Start';
+        testBtn.disabled = false;
+        recognition.stop();
+      }
   }
   
   recognition.onnomatch = function(event) {
@@ -81,19 +104,25 @@ function testSpeech() {
   
   recognition.onsoundstart = function(event) {
       //Fired when any sound — recognisable speech or not — has been detected.
-      console.log('SpeechRecognition.onsoundstart');
+      if (buttonState == 1) {
+        console.log('SpeechRecognition.onsoundstart');
+      }
   }
   
   recognition.onsoundend = function(event) {
       //Fired when any sound — recognisable speech or not — has stopped being detected.
-      console.log('SpeechRecognition.onsoundend');
-      socket.emit('status', 'onsoundend');
+      if (buttonState == 1) {
+        console.log('SpeechRecognition.onsoundend');
+        socket.emit('status', 'onsoundend');
+      }
   }
   
   recognition.onspeechstart = function (event) {
       //Fired when sound that is recognised by the speech recognition service as speech has been detected.
-      console.log('SpeechRecognition.onspeechstart');
-      socket.emit('status', 'onspeechstart');
+      if (buttonState == 1) {
+        console.log('SpeechRecognition.onspeechstart');
+        socket.emit('status', 'onspeechstart');
+      }
   }
   recognition.onstart = function(event) {
       //Fired when the speech recognition service has begun listening to incoming audio with intent to recognize grammars associated with the current SpeechRecognition.
@@ -101,4 +130,4 @@ function testSpeech() {
   }
 }
 
-testBtn.addEventListener('click', testSpeech);
+testBtn.addEventListener('click', speechButton);
